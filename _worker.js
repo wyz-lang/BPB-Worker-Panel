9048212f2ccec664c183109d5bbc887251cf37b4
wyz-lang <wyz4902@gmail.com>
2024-08-15 11:13:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -215,7 +215,7 @@ export default {
 
                     default:
                 	return new Response('Not found', { status: 404 });
-                        url.hostname = '';
+                        url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
